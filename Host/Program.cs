c115b19495cf4fccf69854cdf86ffbba7c715419
aleksei6191@gmail.com <aleksei6191@gmail.com>
2022-04-19 11:40:40 +0300

    add Host

---
+++ Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(Service.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Сервер запущен!");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
