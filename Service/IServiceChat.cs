5d342cb8bdd75061c2f8f12b6bd7ae7f888851f7
aleksei6191@gmail.com <aleksei6191@gmail.com>
2022-04-19 11:23:21 +0300

    Добавьте файлы проекта.

---
+++ Service/IServiceChat.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+
+namespace Service
+{
+    [ServiceContract(CallbackContract =typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string nameUser);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract (IsOneWay = true)]
+        void SendMessage(string message, int id);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MessageCallback(string message);
+    }
+}
---
+++ Service/ServiceChat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Service
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        private List<User> _users = new List<User>();
+        private int _newId = 1;
+
+        public int Connect(string nameUser)
+        {
+            User user = new User() { Id = _newId, Name = nameUser, operationContext = OperationContext.Current };
+            _newId++;
+            _users.Add(user);
+            SendMessage(user.Name + " вошёл в чат!", 0);
+            return user.Id;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = _users.Find(i => i.Id == id);
+            if (user!=null)
+            {
+                _users.Remove(user);
+                SendMessage(user.Name + " вышел из чата!", 0);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            foreach (var item in _users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                var user = _users.Find(i => i.Id == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += message;
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MessageCallback(answer);
+            }
+        }
+    }
+}
